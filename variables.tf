ffd1de2d2a1bd26565b5bb15b34ad966e90779f0
Prem Kumar <pkailasu@sph.com.sg>
2022-11-17 15:28:34 +0800

    use role name to decide whether to create

--- variables.tf
+++ variables.tf
@@ -25,9 +25,9 @@ variable "irsa_config" {
   description = "Input configuration for IRSA module"
   type = object({
     role_name                         = optional(string, "")
-    role_policy_arns                  = optional(map(string), null)
+    role_policy_arns                  = optional(map(string), {})
     create_kubernetes_namespace       = optional(bool, false)
-    create_kubernetes_service_account = optional(bool, true)
+    create_kubernetes_service_account = optional(bool, false)
     kubernetes_namespace              = optional(string, "")
     kubernetes_service_account        = optional(string, "")
     oidc_providers                    = optional(any, null)
